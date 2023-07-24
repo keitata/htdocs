2e01ea9826fc6f6ad05b498b9e83fd623e242fe5
keitata <sakurai.07111@gmail.com>
2023-07-24 10:47:18 +0900

    bmi.php HTML内にphpを。コマンド引数として指定

--- 6/bmi.php
+++ 6/bmi.php
@@ -1,12 +1,24 @@
 <?php
+if (isset($argv[1]) && isset($argv[2])) {
+    $height_cm = (float) $argv[1];
+    $weight_kg = (float) $argv[2];
 
-$height_cm = 170; 
-$weight_kg = 60;  
+    // 身長をメートル単位に変換
+    $height_m = $height_cm / 100;
 
-// BMIの計算
-$height_m = $height_cm / 100; // cmをmに変換
-$bmi = round($weight_kg / ($height_m * $height_m), 2); 
-
-// 結果の表示
-echo "{$height_cm}センチ、{$weight_kg}キログラムのあなたのBMIは{$bmi}です。";
+    // BMIの計算
+    $bmi = round($weight_kg / ($height_m * $height_m), 3);
+} else {
+    $bmi = "エラー：身長と体重をコマンド引数として指定してください";
+}
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>BMI計算結果</title>
+</head>
+<body>
+    <h1>BMI計算結果</h1>
+    <p><?php echo "{$height_cm}センチ、{$weight_kg}キログラムのあなたのBMIは{$bmi}です。"; ?></p>
+</body>
+</html>
