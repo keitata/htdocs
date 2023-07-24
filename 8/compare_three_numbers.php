16cf939c9822a68f1d00a5c67f081b850783ba8d
keitata <sakurai.07111@gmail.com>
2023-07-24 11:10:35 +0900

    課題8追加

--- 8/compare_three_numbers.php
+++ 8/compare_three_numbers.php
@@ -1,13 +1,24 @@
-<?php
-// 乱数を生成して変数に代入（0から100の範囲）
-$a = rand(0, 100);
-$b = rand(0, 100);
-$c = rand(0, 100);
+<!DOCTYPE html>
+<html>
+<head>
+    <title>最大値の判定</title>
+</head>
+<body>
+    <h1>最大値の判定</h1>
 
-// 最大の値を計算
-$max_value = max($a, $b, $c);
+    <?php
+    // 乱数を生成して変数に代入（0から100の範囲）
+    $a = rand(0, 100);
+    $b = rand(0, 100);
+    $c = rand(0, 100);
 
-// 結果の表示
-echo "a: {$a}, b: {$b}, c: {$c}";
-echo "最大値: {$max_value}";
-?>
+    // 最大の値を計算
+    $max_value = max($a, $b, $c);
+    ?>
+
+    <p>a: <?php echo $a; ?></p>
+    <p>b: <?php echo $b; ?></p>
+    <p>c: <?php echo $c; ?></p>
+    <p>最大値: <?php echo $max_value; ?></p>
+</body>
+</html>
