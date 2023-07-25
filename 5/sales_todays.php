f0108c9eb7f3499c89deb342988a4cc058bb125d
keitata <sakurai.07111@gmail.com>
2023-07-25 15:40:56 +0900

    一人当たりの利益計算　修正

--- 5/sales_todays.php
+++ 5/sales_todays.php
@@ -15,21 +15,23 @@ $result = 0;
 $result += $sales_today;
 
 // 今日の売上を出力
-echo '今日の売上は' . $result . 'ドルです。<br>';
+echo '<p>今日の売上は' . $result . 'ドルです。</p>';
 
 // 2. 複合代入演算子でresultからcostを引く
 $result -= $cost;
 
 // 出力
-echo '今日の利益は' . $result . 'ドルです。<br>';
+echo '<p>今日の利益は' . $result . 'ドルです。</p>';
 
 // 3. 複合代入演算子でresultにrateを掛ける
 $result *= $rate;
 
 // 出力
-echo '今日の利益は日本円で' . $result . '円です。<br>';
+echo '<p>今日の利益は日本円で' . $result . '円です。</p>';
 
 // 4. 複合代入演算子でresultをnumber_of_teamで割る
+$result /= $number_of_team;
 
 // 出力
-echo '一人あたりの利益は' . $result . '円です。<br>';
+echo '<p>一人あたりの利益は' . $result . '円です。</p>';
+?>
